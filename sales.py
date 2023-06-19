48b40d5b86b27be6cbba588c8a4650b8216df599
Daniel Ortiz <dani.ortiz.datascience@gmail.com>
2023-06-19 12:42:25 +0100

    Amount and income addeded to products dataframe

--- sales.py
+++ sales.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 # Data load
 products = pd.DataFrame({'Name': ['Shirts', 'Shorts', 'Trousers'], 'Price': [50, 35, 60]})
+
+# Calculate income
+products['Amount'] = [1000, 1500, 4200]
+products['Income'] = products['Price'] * products['Amount']
